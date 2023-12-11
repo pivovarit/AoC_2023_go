2e9a54eabb8e18c19e381b8a11b089783bc76e3a
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2023-12-11 08:02:20 +0100

    Day 11

---
+++ day_11/solution_test.go
@@ -0,0 +1,114 @@
+package day11
+
+import (
+	"testing"
+)
+
+func Test_run(t *testing.T) {
+	run() // 9693756
+}
+
+func Test_cosmicExpansionPart1(t *testing.T) {
+	tests := []struct {
+		name  string
+		input []string
+		want  int
+	}{
+		{name: "example", input: []string{
+			"...#......",
+			".......#..",
+			"#.........",
+			"..........",
+			"......#...",
+			".#........",
+			".........#",
+			"..........",
+			".......#..",
+			"#...#.....",
+		}, want: 374},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := cosmicExpansionPart1(tt.input); got != tt.want {
+				t.Errorf("cosmicExpansionPart1() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_calculateExpandedPath(t *testing.T) {
+	tests := []struct {
+		name  string
+		input []string
+		n     int
+		want  int
+	}{
+		{name: "example", input: []string{
+			"...#......",
+			".......#..",
+			"#.........",
+			"..........",
+			"......#...",
+			".#........",
+			".........#",
+			"..........",
+			".......#..",
+			"#...#.....",
+		}, n: 10, want: 1030},
+		{name: "example 2", input: []string{
+			"...#......",
+			".......#..",
+			"#.........",
+			"..........",
+			"......#...",
+			".#........",
+			".........#",
+			"..........",
+			".......#..",
+			"#...#.....",
+		}, n: 100, want: 8410},
+		{name: "example 3", input: []string{
+			"...#......",
+			".......#..",
+			"#.........",
+			"..........",
+			"......#...",
+			".#........",
+			".........#",
+			"..........",
+			".......#..",
+			"#...#.....",
+		}, n: 1000, want: 82210},
+		{name: "example 4", input: []string{
+			"...#......",
+			".......#..",
+			"#.........",
+			"..........",
+			"......#...",
+			".#........",
+			".........#",
+			"..........",
+			".......#..",
+			"#...#.....",
+		}, n: 10000, want: 820210},
+		{name: "example 5", input: []string{
+			"...#......",
+			".......#..",
+			"#.........",
+			"..........",
+			"......#...",
+			".#........",
+			".........#",
+			"..........",
+			".......#..",
+			"#...#.....",
+		}, n: 100000, want: 8200210},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := calculateExpandedPath(tt.input, tt.n); got != tt.want {
+				t.Errorf("cosmicExpansionPart2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
