b4d5cc80c9ca653d289b8a2ad0810b61b8fe521e
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2023-12-15 22:01:19 +0100

    Day 1 - fix

--- day_1/solution.go
+++ day_1/solution.go
@@ -11,7 +11,7 @@ func hash(s *string) int {
 }
 
 func getNumberStringBuckets() [23]int {
-	return [23]int{8, -1, -2, 1, -1, -1, -1, 9, -1, -1, 3, 8, -1, -1, -1, 5, -1, 6, -1, -1, 4, -1, 2}
+	return [23]int{8, -1, -2, 1, -1, -1, -1, 9, -1, -1, 3, 7, -1, -1, -1, 5, -1, 6, -1, -1, 4, -1, 2}
 }
 
 func run() {
