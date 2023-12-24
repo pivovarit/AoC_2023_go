538bc01591b4f5227f185bbd9c7c44392ae5bdba
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2023-12-24 11:41:07 +0100

    Day 2 - optimized

--- day_2/solution.go
+++ day_2/solution.go
@@ -2,118 +2,327 @@ package day2
 
 import (
 	"github.com/pivovarit/aoc/util"
-	"log"
-	"math"
-	"strconv"
-	"strings"
-)
-
-const (
-	BLUE  = "blue"
-	GREEN = "green"
-	RED   = "red"
+	"runtime"
+	"sync"
 )
 
 func run() {
 	input := util.ReadInput()
 
-	util.Timed("cubeConundrumPart1", func() int {
-		return cubeConundrumPart1(input)
+	util.Timed("cubeConundrumPart1Sequential", func() int {
+		return cubeConundrumPart1Sequential(input)
 	})
-	util.Timed("cubeConundrumPart1", func() int {
-		return cubeConundrumPart2(input)
+	util.Timed("cubeConundrumPart1Parallel", func() int {
+		return cubeConundrumPart1Parallel(input)
+	})
+	util.Timed("cubeConundrumPart2Sequential", func() int {
+		return cubeConundrumPart2Sequential(input)
+	})
+
+	util.Timed("cubeConundrumPart2Parallel", func() int {
+		return cubeConundrumPart2Parallel(input)
 	})
 }
 
-func cubeConundrumPart1(input []string) int {
-	colourMap := map[string]int{
-		RED:   12,
-		GREEN: 13,
-		BLUE:  14,
-	}
+const (
+	redCubesCount   = 12
+	greenCubesCount = 13
+	blueCubesCount  = 14
+)
 
-	idSum := 0
-	for _, str := range input {
-		id, rounds := parseEntry(str)
-		invalid := false
-		for _, round := range rounds {
-			for _, choice := range round.choices {
-				if choice.count > colourMap[choice.colour] {
-					invalid = true
-					break
+type State int
+
+const (
+	processingGameId State = iota
+	processingCubeCount
+	processingCubeColour
+)
+
+const (
+	emptySeparator      = ' '
+	gameIdSeparator     = ':'
+	cubeChoiceSeparator = ','
+	gameSeparator       = ';'
+)
+
+func cubeConundrumPart1Sequential(input []string) (sum int) {
+LineLoop:
+	for _, line := range input {
+		state := processingGameId
+		gameId := 0
+		cubeCount := 0
+		cubeColourProcessed := false
+		colourPrefix := true
+		for _, char := range line[5:] {
+			switch state {
+			case processingGameId:
+				if char != gameIdSeparator {
+					gameId = gameId*10 + int(char-'0')
+				} else {
+					state = processingCubeCount
+				}
+			case processingCubeCount:
+				switch {
+				case char == emptySeparator && colourPrefix:
+					colourPrefix = false
+
+				case char != emptySeparator:
+					cubeCount = cubeCount*10 + int(char-'0')
+
+				case char == emptySeparator && !colourPrefix:
+					state = processingCubeColour
+				}
+			case processingCubeColour:
+				if char != cubeChoiceSeparator && char != gameSeparator {
+					if cubeColourProcessed {
+						continue
+					}
+					switch char {
+					case 'r':
+						if cubeCount > redCubesCount {
+							continue LineLoop
+						}
+					case 'b':
+						if cubeCount > blueCubesCount {
+							continue LineLoop
+						}
+					case 'g':
+						if cubeCount > greenCubesCount {
+							continue LineLoop
+						}
+					}
+					cubeColourProcessed = true
+				} else {
+					cubeColourProcessed = false
+					cubeCount = 0
+					state = processingCubeCount
+					colourPrefix = true
 				}
 			}
 		}
-		if !invalid {
-			idSum += id
-		}
+		sum += gameId
 	}
-	return idSum
+	return sum
 }
 
-type GameRound struct {
-	choices []GameEntry
-}
+func cubeConundrumPart1Parallel(input []string) (sum int) {
+	var wg sync.WaitGroup
+	parallelism := runtime.NumCPU()
+	inputLen := len(input)
+	parallelism = min(inputLen, parallelism)
 
-type GameEntry struct {
-	count  int
-	colour string
-}
+	var idSumPerCPU = make([]int, parallelism)
 
-func Entry(count int, colour string) GameEntry {
-	return GameEntry{
-		count:  count,
-		colour: colour,
-	}
-}
+	for cpuId := 0; cpuId < parallelism; cpuId++ {
+		wg.Add(1)
+		go func(cpu int) {
+			defer wg.Done()
+		LineLoop:
+			for i := cpu; i < inputLen; i = i + parallelism {
+				line := input[i]
+				state := processingGameId
+				gameId := 0
+				cubeCount := 0
+				cubeColourProcessed := false
+				colourPrefix := true
+				for _, char := range line[5:] {
+					switch state {
+					case processingGameId:
+						if char != gameIdSeparator {
+							gameId = gameId*10 + int(char-'0')
+						} else {
+							state = processingCubeCount
+						}
+					case processingCubeCount:
+						if char == emptySeparator && colourPrefix {
+							colourPrefix = false
+							break
+						}
 
-func Round(entries ...GameEntry) GameRound {
-	return GameRound{choices: entries}
-}
+						if char != emptySeparator {
+							cubeCount = cubeCount*10 + int(char-'0')
+							break
+						}
 
-func parseEntry(input string) (int, []GameRound) {
-	split := strings.Split(input, ":")
-	id, err := strconv.Atoi(split[0][5:])
-	if err != nil {
-		log.Panicf("illegal input format")
-	}
-
-	var rounds []GameRound
-
-	rawRounds := strings.Split(strings.TrimSpace(split[1]), ";")
-
-	for _, round := range rawRounds {
-		var entries []GameEntry
-		rawEntries := strings.Split(round, ",")
-		for _, entry := range rawEntries {
-			parsedEntry := strings.Split(strings.TrimSpace(entry), " ")
-			count, err := strconv.Atoi(strings.TrimSpace(parsedEntry[0]))
-			if err != nil {
-				panic("illegal game round format")
+						if char == emptySeparator && !colourPrefix {
+							state = processingCubeColour
+						}
+					case processingCubeColour:
+						if char != cubeChoiceSeparator && char != gameSeparator {
+							if cubeColourProcessed {
+								continue
+							}
+							switch char {
+							case 'r':
+								if cubeCount > redCubesCount {
+									continue LineLoop
+								}
+							case 'b':
+								if cubeCount > blueCubesCount {
+									continue LineLoop
+								}
+							case 'g':
+								if cubeCount > greenCubesCount {
+									continue LineLoop
+								}
+							}
+							cubeColourProcessed = true
+						} else {
+							cubeColourProcessed = false
+							cubeCount = 0
+							state = processingCubeCount
+							colourPrefix = true
+						}
+					}
+				}
+				idSumPerCPU[cpu] += gameId
 			}
-
-			colour := strings.TrimSpace(parsedEntry[1])
-
-			entries = append(entries, Entry(count, colour))
-		}
-
-		rounds = append(rounds, Round(entries...))
+		}(cpuId)
 	}
 
-	return id, rounds
+	wg.Wait()
+
+	for _, sumPerCpu := range idSumPerCPU {
+		sum += sumPerCpu
+	}
+
+	return sum
 }
 
-func cubeConundrumPart2(input []string) int {
-	sum := 0
-	for _, str := range input {
-		maxCubes := map[string]int{}
-		_, rounds := parseEntry(str)
-		for _, round := range rounds {
-			for _, entry := range round.choices {
-				maxCubes[entry.colour] = int(math.Max(float64(maxCubes[entry.colour]), float64(entry.count)))
+func cubeConundrumPart2Sequential(input []string) (sum int) {
+	for _, line := range input {
+		state := processingGameId
+		gameId := 0
+		cubeCount := 0
+		cubeColourProcessed := false
+		colourPrefix := true
+		maxGreen := -1
+		maxBlue := -1
+		maxRed := -1
+		for _, char := range line[5:] {
+			switch state {
+			case processingGameId:
+				if char != gameIdSeparator {
+					gameId = gameId*10 + int(char-'0')
+				} else {
+					state = processingCubeCount
+				}
+			case processingCubeCount:
+				if char == emptySeparator && colourPrefix {
+					colourPrefix = false
+					break
+				}
+
+				if char != emptySeparator {
+					cubeCount = cubeCount*10 + int(char-'0')
+					break
+				}
+
+				if char == emptySeparator && !colourPrefix {
+					state = processingCubeColour
+				}
+			case processingCubeColour:
+				if char != cubeChoiceSeparator && char != gameSeparator {
+					if cubeColourProcessed {
+						continue
+					}
+					switch char {
+					case 'r':
+						maxRed = max(maxRed, cubeCount)
+					case 'b':
+						maxBlue = max(maxBlue, cubeCount)
+					case 'g':
+						maxGreen = max(maxGreen, cubeCount)
+					}
+					cubeColourProcessed = true
+				} else {
+					cubeColourProcessed = false
+					cubeCount = 0
+					state = processingCubeCount
+					colourPrefix = true
+				}
 			}
 		}
-		sum += maxCubes[RED] * maxCubes[GREEN] * maxCubes[BLUE]
+		sum += maxBlue * maxGreen * maxRed
 	}
+	return sum
+}
+
+func cubeConundrumPart2Parallel(input []string) (sum int) {
+	var wg sync.WaitGroup
+	parallelism := runtime.NumCPU()
+	inputLen := len(input)
+	parallelism = min(inputLen, parallelism)
+
+	var powerPerCPU = make([]int, parallelism)
+
+	for cpuId := 0; cpuId < parallelism; cpuId++ {
+		wg.Add(1)
+		go func(cpu int) {
+			defer wg.Done()
+			for i := cpu; i < inputLen; i = i + parallelism {
+				line := input[i]
+				state := processingGameId
+				gameId := 0
+				cubeCount := 0
+				cubeColourProcessed := false
+				colourPrefix := true
+				maxGreen := -1
+				maxBlue := -1
+				maxRed := -1
+				for _, char := range line[5:] {
+					switch state {
+					case processingGameId:
+						if char != gameIdSeparator {
+							gameId = gameId*10 + int(char-'0')
+						} else {
+							state = processingCubeCount
+						}
+					case processingCubeCount:
+						if char == emptySeparator && colourPrefix {
+							colourPrefix = false
+							break
+						}
+
+						if char != emptySeparator {
+							cubeCount = cubeCount*10 + int(char-'0')
+							break
+						}
+
+						if char == emptySeparator && !colourPrefix {
+							state = processingCubeColour
+						}
+					case processingCubeColour:
+						if char != cubeChoiceSeparator && char != gameSeparator {
+							if cubeColourProcessed {
+								continue
+							}
+							switch char {
+							case 'r':
+								maxRed = max(maxRed, cubeCount)
+							case 'b':
+								maxBlue = max(maxBlue, cubeCount)
+							case 'g':
+								maxGreen = max(maxGreen, cubeCount)
+							}
+							cubeColourProcessed = true
+						} else {
+							cubeColourProcessed = false
+							cubeCount = 0
+							state = processingCubeCount
+							colourPrefix = true
+						}
+					}
+				}
+				powerPerCPU[cpu] += maxBlue * maxGreen * maxRed
+			}
+		}(cpuId)
+	}
+	wg.Wait()
+
+	for _, sumPerCpu := range powerPerCPU {
+		sum += sumPerCpu
+	}
+
 	return sum
 }
--- day_2/solution_test.go
+++ day_2/solution_test.go
@@ -2,7 +2,6 @@ package day2
 
 import (
 	"github.com/pivovarit/aoc/util"
-	"reflect"
 	"testing"
 )
 
@@ -12,19 +11,31 @@ func Test_run(t *testing.T) {
 	run()
 }
 
-func BenchmarkCubeConundrumPart1(b *testing.B) {
+func BenchmarkCubeConundrumPart1Sequential(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		cubeConundrumPart1(input)
+		cubeConundrumPart1Sequential(input)
 	}
 }
 
-func BenchmarkCubeConundrumPart2(b *testing.B) {
+func BenchmarkCubeConundrumPart1Parallel(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		cubeConundrumPart2(input)
+		cubeConundrumPart1Parallel(input)
 	}
 }
 
-func Test_cubeConundrum(t *testing.T) {
+func BenchmarkCubeConundrumPart2Sequential(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		cubeConundrumPart2Sequential(input)
+	}
+}
+
+func BenchmarkCubeConundrumPart2Parallel(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		cubeConundrumPart2Parallel(input)
+	}
+}
+
+func Test_cubeConundrumPart1_sequential(t *testing.T) {
 	type args struct {
 		input []string
 	}
@@ -48,14 +59,45 @@ func Test_cubeConundrum(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := cubeConundrumPart1(tt.args.input); got != tt.want {
-				t.Errorf("cubeConundrumPart1() = %v, want %v", got, tt.want)
+			if got := cubeConundrumPart1Sequential(tt.args.input); got != tt.want {
+				t.Errorf("cubeConundrumPart1_sequential() = %v, want %v", got, tt.want)
 			}
 		})
 	}
 }
 
-func Test_cubeConundrumPart2(t *testing.T) {
+func Test_cubeConundrumPart2_parallel(t *testing.T) {
+	type args struct {
+		input []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{name: "Game 1", args: args{[]string{"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"}}, want: 1},
+		{name: "Game 2", args: args{[]string{"Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue"}}, want: 2},
+		{name: "Game 3", args: args{[]string{"Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red"}}, want: 0},
+		{name: "Game 4", args: args{[]string{"Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red"}}, want: 0},
+		{name: "Game 5", args: args{[]string{"Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"}}, want: 5},
+		{name: "example", args: args{[]string{
+			"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+			"Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+			"Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+			"Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+			"Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
+		}}, want: 8},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := cubeConundrumPart1Parallel(tt.args.input); got != tt.want {
+				t.Errorf("cubeConundrumPart1_parallel() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_cubeConundrumPart2Sequential(t *testing.T) {
 	type args struct {
 		input []string
 	}
@@ -79,76 +121,39 @@ func Test_cubeConundrumPart2(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := cubeConundrumPart2(tt.args.input); got != tt.want {
-				t.Errorf("cubeConundrumPart2() = %v, want %v", got, tt.want)
+			if got := cubeConundrumPart2Sequential(tt.args.input); got != tt.want {
+				t.Errorf("cubeConundrumPart2Sequential() = %v, want %v", got, tt.want)
 			}
 		})
 	}
 }
 
-func Test_parseEntry(t *testing.T) {
+func Test_cubeConundrumPart2Parallel(t *testing.T) {
 	type args struct {
-		input string
+		input []string
 	}
 	tests := []struct {
-		name       string
-		args       args
-		want       int
-		wantRounds []GameRound
+		name string
+		args args
+		want int
 	}{
-		{
-			name: "Game 1",
-			args: args{"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"},
-			want: 1,
-			wantRounds: []GameRound{
-				Round(Entry(3, BLUE), Entry(4, RED)),
-				Round(Entry(1, RED), Entry(2, GREEN), Entry(6, BLUE)),
-				Round(Entry(2, GREEN)),
-			}},
-		{
-			name: "Game 2",
-			args: args{"Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue"},
-			want: 2,
-			wantRounds: []GameRound{
-				Round(Entry(1, BLUE), Entry(2, GREEN)),
-				Round(Entry(3, GREEN), Entry(4, BLUE), Entry(1, RED)),
-				Round(Entry(1, GREEN), Entry(1, BLUE)),
-			}},
-		{
-			name: "Game 3",
-			args: args{"Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red"},
-			want: 3,
-			wantRounds: []GameRound{
-				Round(Entry(8, GREEN), Entry(6, BLUE), Entry(20, RED)),
-				Round(Entry(5, BLUE), Entry(4, RED), Entry(13, GREEN)),
-				Round(Entry(5, GREEN), Entry(1, RED)),
-			}},
-		{
-			name: "Game 4",
-			args: args{"Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red"},
-			want: 4,
-			wantRounds: []GameRound{
-				Round(Entry(1, GREEN), Entry(3, RED), Entry(6, BLUE)),
-				Round(Entry(3, GREEN), Entry(6, RED)),
-				Round(Entry(3, GREEN), Entry(15, BLUE), Entry(14, RED)),
-			}},
-		{
-			name: "Game 5",
-			args: args{"Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"},
-			want: 5,
-			wantRounds: []GameRound{
-				Round(Entry(6, RED), Entry(1, BLUE), Entry(3, GREEN)),
-				Round(Entry(2, BLUE), Entry(1, RED), Entry(2, GREEN)),
-			}},
+		{name: "Game 1", args: args{[]string{"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"}}, want: 48},
+		{name: "Game 2", args: args{[]string{"Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue"}}, want: 12},
+		{name: "Game 3", args: args{[]string{"Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red"}}, want: 1560},
+		{name: "Game 4", args: args{[]string{"Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red"}}, want: 630},
+		{name: "Game 5", args: args{[]string{"Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"}}, want: 36},
+		{name: "example", args: args{[]string{
+			"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+			"Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+			"Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+			"Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+			"Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
+		}}, want: 2286},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, got1 := parseEntry(tt.args.input)
-			if got != tt.want {
-				t.Errorf("parseEntry() got = %v, want %v", got, tt.want)
-			}
-			if !reflect.DeepEqual(got1, tt.wantRounds) {
-				t.Errorf("parseEntry() got = %v, want %v", got1, tt.wantRounds)
+			if got := cubeConundrumPart2Parallel(tt.args.input); got != tt.want {
+				t.Errorf("cubeConundrumPart2Parallel() = %v, want %v", got, tt.want)
 			}
 		})
 	}
